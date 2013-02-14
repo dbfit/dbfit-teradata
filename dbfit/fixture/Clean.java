97cd202e081bbe3f91a90db0f2c7fce690fd34cf
Paul Brian (lifeisstillgood) <paul@mikadosoftware.com>
2013-02-14 18:54:58 +0000

    Adding GPL header to relevant files

--- dbfit/fixture/Clean.java
+++ dbfit/fixture/Clean.java
@@ -1,3 +1,13 @@
+
+/**
+ * Copyright (c) Mark Matten (mark_matten@hotmail.com) 2011-13
+ * Part of the dbfit-teradata software.
+ * This software is subject to the provisions of
+ * the GNU General Public License Version 2.0 (GPL).
+ * See LICENCE.txt for details.
+ */
+
+
 package dbfit.fixture;
 
 import java.math.BigDecimal;
--- dbfit/util/SymbolUtil.java
+++ dbfit/util/SymbolUtil.java
@@ -1,3 +1,13 @@
+
+/**
+ * Copyright (c) Mark Matten (mark_matten@hotmail.com) 2011-13
+ * Part of the dbfit-teradata software.
+ * This software is subject to the provisions of
+ * the GNU General Public License Version 2.0 (GPL).
+ * See LICENCE.txt for details.
+ */
+
+
 package dbfit.util;
 /** ugly workaround for fit change in release 200807, which internally converts NULL into a string value "null";
  * for db access, we need to make a difference between NULL and "null" so this class provides a centralised
